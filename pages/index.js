3cef1c4573eb7beb9f31631d1df062b4f52835e0
Mukul Periwal <mukulperiwal@Mukuls-MacBook-Pro.local>
2023-09-06 20:35:18 -0400

    added twice reload feature

--- pages/index.js
+++ pages/index.js
@@ -4,23 +4,34 @@ import Contact from '../components/Contact';
 import Main from '../components/Main';
 import Projects from '../components/Projects';
 import Skills from '../components/Skills';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Mukul | Software Engineer</title>
-        <meta
-          name="description"
-          content="I’m a software engineer, doing my Physics major/CS-Math minor minor at Drexel University."
-        />
-        <link rel="icon" href="/fav.png" />
-      </Head>
-      <Main />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-    </div>
-  );
+    const [reloadCount, setReloadCount] = useState(0);
+
+    useEffect(() => {
+        // This effect will run when the component mounts and each time reloadCount changes.
+        // To make it run twice, increment reloadCount and set it to 2.
+        if (reloadCount < 2) {
+            setReloadCount(reloadCount + 1);
+        }
+    }, [reloadCount]); // Add reloadCount as a dependency
+
+    return (
+        <div>
+            <Head>
+                <title>Mukul | Software Engineer</title>
+                <meta
+                    name="description"
+                    content="I’m a software engineer, doing my Physics major/CS-Math minor minor at Drexel University."
+                />
+                <link rel="icon" href="/fav.png" />
+            </Head>
+            <Main />
+            <About />
+            <Skills />
+            <Projects />
+            <Contact />
+        </div>
+    );
 }
